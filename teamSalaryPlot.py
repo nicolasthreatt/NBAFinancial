da691d496a0a78151ed4924a31c34532fe6e7630
Nicolas Threatt <nicolasthreattcu@gmail.com>
2020-10-07 21:06:34 -0500

    10.7.2020 - Individual team plot finished

--- teamSalaryPlot.py
+++ teamSalaryPlot.py
@@ -2,7 +2,7 @@
 teamContracts.py
 
 TODO:
-    - Finish Command line arguments for year and team
+    - Finish Command line arguments for individual year
     - Cleanp
 '''
 
@@ -15,6 +15,7 @@ import numpy as np
 import os
 import pandas as pd
 import pyodbc
+import sys
 
 
 class Team(dict):
@@ -38,46 +39,70 @@ def getFutureSeason(currentSeason, futureSeason):
             "Guaranteed")
 
 
-def getTeamsSalaryCapData(season):
+def getCmdTeam(cmdTeamAbr):
+    teams = [
+        ("ATL", "Atlanta Hawks"),          ("BOS", "Boston Celtics"),     ("BRK", "Brooklyn Nets"), 
+        ("CHO", "Charlotte Hornets"),      ("CHI", "Chicago Bulls"),      ("CLE", "Cleveland Cavaliers"), 
+        ("DAL", "Dallas Mavericks"),       ("DEN", "Denver Nuggets"),     ("DET", "Detroit Pistons"), 
+        ("GSW", "Golden State Warriors"),  ("HOU", "Houston Rockets"),    ("IND", "Indiana Pacers"),
+        ("LAC", "Los Angeles Clippers"),   ("LAL", "Los Angeles Lakers"), ("MEM", "Memphis Grizzlies"),
+        ("MIA", "Miami Heat"),             ("MIL", "Milwaukee Bucks"),    ("MIN", "Minnesota Timberwolves"),
+        ("NOP", "New Orleans Pelicans"),   ("NYK", "New York Knicks"),    ("OKC", "Oklahoma City Thunder"),
+        ("ORL", "Orlando Magic"),          ("PHI", "Philadelphia 76ers"), ("PHO", "Phoenix Suns"),
+        ("POR", "Portland Trail Blazers"), ("SAC", "Sacramento Kings"),   ("SAS", "San Antonio Spurs"),
+        ("TOR", "Toronto Raptors"),        ("UTA", "Utah Jazz"),          ("WAS", "Washington Wizards"),
+    ]
+
+    for team in teams:
+        if team[0] == cmdTeamAbr:
+            return [team[1]]
+
+    sys.exit('Invalid Team!')
+
+
+def getTeamsSalaryCapData(season, cmdTeamAbr):
 
     sql_table_df = financialDB.readTable("Players", "Payroll{}".format(season))
 
     currentSeason = int(season[:4])
     futureSeason  = int(season[-2:])
-    contractYears = (year1, year2, year3, year4, year5, year6, guaranteed) = getFutureSeason(currentSeason, futureSeason)
+    contractYears = getFutureSeason(currentSeason, futureSeason)
 
-    for season in contractYears:
+    for year in contractYears:
 
         # Format Data
-        sql_table_df[season] = sql_table_df[season].map(lambda x: x.replace(",", "").replace("$", ""))
-        sql_table_df[season] = [0 if contract == '' else contract for contract in sql_table_df[season]]
-        sql_table_df[season] = sql_table_df[season].astype(int)
+        sql_table_df[year] = sql_table_df[year].map(lambda x: x.replace(",", "").replace("$", ""))
+        sql_table_df[year] = [0 if contract == '' else contract for contract in sql_table_df[year]]
+        sql_table_df[year] = sql_table_df[year].astype(int)
+
+    teams = getCmdTeam(cmdTeamAbr) if cmdTeamAbr else sql_table_df['Team'].unique()
 
     teamsPlayersContracts = dict()
-    for team in sql_table_df['Team'].unique():
+    for team in teams:
 
         teamsPlayersContracts[team] = Team()
         for year in contractYears:
 
             year_sorted = sql_table_df[['Player', year]].sort_values(by=[year])
             year_sorted = year_sorted[year_sorted[year] > 0]
-            teamsPlayersContracts[team].seasons[year] = ContractInfo( players=year_sorted["Player"].loc[sql_table_df.Team == team], 
-                                                                      contracts=year_sorted[year].loc[sql_table_df.Team == team] )
+            teamsPlayersContracts[team].seasons[year] = ContractInfo(players=year_sorted["Player"].loc[sql_table_df.Team == team], 
+                                                                     contracts=year_sorted[year].loc[sql_table_df.Team == team])
 
-    createPlots(teamsPlayersContracts)
+    createPlots(teamsPlayersContracts, season, cmdTeamAbr)
 
 
-def createPlots(df_dict, season="2019-20"):
+def createPlots(df_dict, season, cmdTeamAbr):
 
-    (currentSeason, futureSeason) = ( int(season[:4]), int(season[-2:]) )
-    contractYears = (year1, year2, year3, year4, year5, year6, guaranteed) = getFutureSeason(currentSeason, futureSeason)
+    (currentSeason, futureSeason) = (int(season[:4]), int(season[-2:]))
+    contractYears = getFutureSeason(currentSeason, futureSeason)
 
-    nrow, ncol = 3, 3
-    for team in df_dict.keys():
+    teams = getCmdTeam(cmdTeamAbr) if cmdTeamAbr else df_dict.keys()
 
-        fig, ax = plt.subplots(nrow, ncol, constrained_layout=True)
+    for team in teams:
+
+        fig, ax = plt.subplots(nrows=3, ncols=3, constrained_layout=True)
         fig.suptitle('Salary Cap Player Breakdown - {}'.format(team), fontsize=12)
-    
+
         seasons = list(contractYears)
 
         for row in range(0, 3):
@@ -106,7 +131,7 @@ def createPlots(df_dict, season="2019-20"):
                                         autopct=lambda p: '{:.1f}%'.format(p) if p > 0 else '',
                                         shadow=False)[0]
                     ax[row, col].set_title(salay_year)
-                    
+
                 else:
                     ax[row, col].axis('off')
 
@@ -121,8 +146,7 @@ def processCmdArgs():
                          help="Teams' Season for Salary Cap Infomation")
     
     parser.add_argument('--team', dest='team', type=str, metavar='', required=False,
-                         help="Individal Team Season Salary Cap Infomation")
-    
+                         help="Abbreviated Team City")
 
     return parser.parse_args()
 
@@ -130,6 +154,5 @@ def processCmdArgs():
 if __name__ == "__main__":
     args = processCmdArgs()
 
-    getTeamsSalaryCapData(args.season)
-
+    getTeamsSalaryCapData(args.season, args.team)
     
